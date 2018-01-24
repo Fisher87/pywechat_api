dd5f19d7c6c8bcd47ab1fda706b5ff46e76d4e13
fisher87 <yulianghua@outlook.com>
2018-01-24 14:50:05 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,10 +10,6 @@ except ImportError:
 setup(
         name='pywechat',
         version='1.0',
-        description= 'BDTT company wechat spider api.',
-        url = 'http://gitlab.benditoutiao.com/bdtt/pywechat', 
-        author = 'bdtt_data_group',
-        author_email = 'btdd@benditoutiao.com',
         classifiers=[ 'Programming Language :: Python :: 2.7',],
         include_package_data=True,
         packages = find_packages()
